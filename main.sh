1ce3d4f07423ecdc967dfe2f92f94c2703e613be
EmmaMathieu <emmaleamathieu@gmail.com>
2023-11-27 22:07:10 +0100

    fvtbhbv

--- main.sh
+++ main.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# le caractère "#!" veut dire que c'est une instruction système, le /bin/bash est le répertoire d'execution pour bash
+# C'est le shebang : le caractère "#!" veut dire que c'est une instruction système qui dit que c'est un script mais pas un fichier binaire, 
+# Le /bin/bash est le répertoire d'execution pour bash ou la localisation de l'interpréteur permettant d'executer le script
 
 
-
-# Petite presentation du programme avec quelques indications
 
 echo -e "\nBienvenue dans ce programme qui analyse les variants génétiques.\nVous avez à disposition un dossier Data dans lequel vous pouvez mettre vos échantillons et vos réplicats au format VCF.\nAvec un nom comme ceci : P+numéro_echantillon-numéro_réplicat.vcf.\nCe programe vous donneras le nombre de variants uniquement pour une insertion connue et commune entre chaque réplicats deux à deux.\nSoit par :\n\t\t1) Position Identique.\n\t\t2) Position +/-10b.\n\t\t3) Avec une positon +/- 10b mais seulement lorsque les séquences se ressemblent à 75%.\n"
-
 # -e pour echo permet l'interpretation par bash des chars {'\t' (permet une tabulation) et '\n' (permet de mettre a la ligne)} 
 
 if [ $# -ne 1 ] || [ $1 = "-h" ]; then
@@ -15,14 +13,13 @@ if [ $# -ne 1 ] || [ $1 = "-h" ]; then
 fi
 # Vérifie si un argument (chemin vers le dossier Data) est passé en ligne de commande
 # Si le cardinal (#) des arguments ($) est différent de (-ne) 1, alors print ce qui est écrit
+# Ou (||) si le premier argument est égal à "-h" alors afficher l'aide
 # Le exit sert a sortir du programme et ne pas faire la suite du programme
 
 racine="$1"
 
-# Appel du script parcourir.py avec le chemin en argument
-#python3 parcourir.py "$racine"
-
 # Appel du script compare.py
 python3 compare.py "$racine"
+
 #python3 parcourir.py "$racine"
 # Cree par Emma MATHIEU : https://github.com/EmmaMathieu
--- parcourir.py
+++ parcourir.py
@@ -1,27 +1,37 @@
 import os
 import sys
 
-def parc(chemin) -> dict:
-    echantillon_et_replicats = {}
+# Cette fonction prend en entrée un chemin de type string et renvoie un dictionnaire contenant des échantillons et le path+leurs réplicats associés.
+def parc(chemin : str) -> dict:
+    echantillon_et_replicats = {}  # Initialise un dictionnaire vide pour stocker les échantillons et leurs réplicats
+    
+    # Parcours de tous les fichiers et dossiers à partir du chemin donné
     for racine, dossiers, fichiers in os.walk(chemin):
+        # Parcours de chaque fichier dans les fichiers de chaque dossier
         for fichier in fichiers:
+            # Vérifie si le fichier a l'extension ".vcf"
             if fichier.endswith(".vcf"):
-                chemin_complet = os.path.join(racine, fichier)
+                chemin_complet = os.path.join(racine, fichier)  # Chemin complet du fichier
                 
-                nom_fichier = os.path.splitext(fichier)[0]
-                elements = nom_fichier.split("-")
+                nom_fichier = os.path.splitext(fichier)[0]  # Récupère le nom du fichier sans extension
+                elements = nom_fichier.split("-")  # Sépare le nom en éléments en utilisant le caractère "-"
                 
+                # Vérifie s'il y a deux éléments dans la séparation
                 if len(elements) == 2:
-                    echantillon, numero_replicat = elements
+                    echantillon, numero_replicat = elements  # Sépare le nom de l'échantillon et le numéro de réplicat
                     
-                    chemin_complet_vcf = os.path.join(racine, fichier)
+                    chemin_complet_vcf = os.path.join(racine, fichier)  # Chemin complet du fichier VCF
                     
+                    # Vérifie si l'échantillon existe déjà dans le dictionnaire
                     if echantillon in echantillon_et_replicats:
+                        # Ajoute le chemin complet du fichier VCF à la liste des fichiers pour cet échantillon
                         echantillon_et_replicats[echantillon].append(chemin_complet_vcf)
                     else:
+                        # Crée une nouvelle entrée dans le dictionnaire pour cet échantillon
                         echantillon_et_replicats[echantillon] = [chemin_complet_vcf]
 
-    return echantillon_et_replicats
+    return echantillon_et_replicats  # Renvoie le dictionnaire contenant les échantillons et leurs fichiers VCF associés
+
 
 #fonction principale pour pouvoir appeler les autres fonction dans le script
 def main():
